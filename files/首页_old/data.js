c84e03b74abd10933cfcec2c26ba1fd8b0f9bbbd
yaoleifly <yaoleifly@gmail.com>
2017-02-15 15:14:25 +0800

    my axure demo of DXY

---
+++ files/首页_old/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q),r,_(),s,_(t,u,v,w,x,y,z,_(),A,_(B,C,D,E,F,_(G,H,I,J),K,null,L,E,M,E,N,O,P,null,Q,R,S,T,U,V,W,R),X,_(),Y,_(),Z,_(ba,[])),bb,_(),bc,_());}; 
+var b="url",c="首页_old.html",d="generationDate",e=new Date(1487142654129.43),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="defaultBackgroundImageInfo",l="path",m="images/首页_old/bg.png",n="width",o=1376,p="height",q=3943,r="viewIdToBackgroundImageInfo",s="page",t="packageId",u="eb8ad924bb7248719ff0aae8ec365995",v="type",w="Axure:Page",x="name",y="首页 old",z="notes",A="style",B="baseStyle",C="627587b6038d43cca051c114ac41ad32",D="pageAlignment",E="near",F="fill",G="fillType",H="solid",I="color",J=0xFFFFFFFF,K="image",L="imageHorizontalAlignment",M="imageVerticalAlignment",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="微软雅黑",W="borderWidth",X="adaptiveStyles",Y="interactionMap",Z="diagram",ba="objects",bb="masters",bc="objectPaths";
+return _creator();
+})());
